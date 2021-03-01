0a16a661779e2f29b5cf744e544a2741c64c8aa3
kkjamba <air.safin@mail.ru>
2021-03-01 16:14:39 +0300

    Initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-_mIfmXfxZydIr5sSyiVu6p_LMiG6L8ZDw&usqp=CAU'></img>
+            </div>
+            <div>
+                ava + description
+        </div>
+            <div>
+                My posts
+          <div>
+                    New post
+          </div>
+                <div>
+                    post 1
+          </div>
+                <div>
+                    post 2
+          </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
