279caede58ecc5211e12acd9310b286cca45a32b
kkjamba <air.safin@mail.ru>
2021-03-02 12:48:14 +0300

    lesson18

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,29 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className='dialog'>
+                    Саша
+                </div>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Петя
+                </div>
+                <div className={s.dialog}>
+                    Вася
+                </div>
+                <div className={s.dialog}>
+                    Виктор
+                </div>
+                <div className={s.dialog}>
+                    Валера
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>how are you?</div>
+                <div className={s.message}>how old are you?</div>
+            </div>
         </div>
     )
 }
