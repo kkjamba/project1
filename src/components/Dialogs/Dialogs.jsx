6dd47fa9eff1a20e2cd9e5c82e0e17d4c7a9062f
kkjamba <air.safin@mail.ru>
2021-03-14 19:11:57 +0300

    lesson27

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,44 +1,30 @@
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
-
-const DialogItem = (props) => {
-    let path = '/dialogs/'+ props.id
-    return (
-        <div className='dialog'>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.message}>{props.message}</div>
-    )
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
     let dialogs = [
-        {id:1, name: 'Саша'},
-        {id:2, name: 'Дима'},
-        {id:3, name: 'Вася'},
-        {id:4, name: 'Виктор'},
-        {id:5, name: 'Валера'}
+        {id: 1, name: 'Саша'},
+        {id: 2, name: 'Дима'},
+        {id: 3, name: 'Вася'},
+        {id: 4, name: 'Виктор'},
+        {id: 5, name: 'Валера'}
     ]
 
     let messages = [
-        {id:1, message: 'Hi'},
-        {id:2, message: 'How are you?'},
-        {id:3, message: 'How old are you?'},
-        {id:4, message: 'Hello'},
-        {id:5, message: 'Yo'}
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'How old are you?'},
+        {id: 4, message: 'Hello'},
+        {id: 5, message: 'Yo'}
     ]
 
-    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id} />)
-    let messagesElements = messages.map( m => <Message message={m.message} /> )
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = messages.map(m => <Message message={m.message}/>)
 
     return (
         <div className={s.dialogs}>
-            <div className={s.dialogsItems} >
+            <div className={s.dialogsItems}>
                 {dialogsElements}
             </div>
             <div className={s.messages}>
